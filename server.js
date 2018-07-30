bbc7e8714ff058efb38ac0b346538bc776b9ef40
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2018-07-30 23:37:03 +0100

    Deployed changes

--- server.js
+++ server.js
@@ -37,11 +37,11 @@ app.use('/api/posts', posts)
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.bind(express.static('client/build'))
+  app.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 const port = process.env.PORT || 5000
