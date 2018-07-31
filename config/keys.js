79f3c78223ded5a797f3a5f4ebeb083c1780d9b5
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2018-07-31 19:52:38 +0100

    Deploy changes

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
+  console.log('In production')
   module.exports = require('./keys_prod')
 } else {
+  console.log('In development')
   module.exports = require('./keys_dev')
 }
